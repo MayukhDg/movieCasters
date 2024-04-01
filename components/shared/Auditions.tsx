6e4a78693e76960343c71fad444563d5250994df
Mayukh <mayukh1995@gmail.com>
2024-04-01 17:42:32 +0530

    slight changes

--- components/shared/Auditions.tsx
+++ components/shared/Auditions.tsx
@@ -10,8 +10,8 @@ const Auditions = async() => {
 
     return (
     <div className='flex flex-wrap w-full items-center gap-5' >
-     { auditions.map((item:any)=>(
-      <AuditionCard key={item._id} title={item.title} />
+     {auditions?.map((item:any)=>(
+      <AuditionCard key={item?._id} title={item?.title} />
      )) }
     </div>
   )
