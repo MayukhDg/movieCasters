3479c2d1d83395eca6110a40ae9b0ed99de5ffff
Mayukh <mayukh1995@gmail.com>
2024-04-05 16:46:09 +0530

    redirects

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import { currentUser } from '@clerk/nextjs';
+import { currentUser, useAuth } from '@clerk/nextjs';
 import Auditions from '@/components/shared/Auditions';
 import { updateUser } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation'
 
 
 
 const Home = async() => {
  
   const user = await currentUser();
+  const { isLoaded } = useAuth();
 
-  if(!user) return null;
+  if(isLoaded){
+    redirect("/")
+  }
+
 
   const loggedInUser = await updateUser({
-    userName: user.username || "", 
-    email:user.emailAddresses[0].emailAddress, 
-    clerkId: user.id, 
-    image:user.imageUrl
+    userName: user?.username || "", 
+    email:user?.emailAddresses[0].emailAddress || "", 
+    clerkId: user?.id || "", 
+    image:user?.imageUrl || ""  
   })
 
 return (
